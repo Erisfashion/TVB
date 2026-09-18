fcd127bdb3d311b312fb99d9048b09882701e358
Erisfashion <erisfashion@gmail.com>
2026-09-18 10:30:25 +0800

    Refactor App class and remove unused code
    Refactor App class to extend Application instead of MultiDexApplication, and remove unused methods and imports.

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -1,114 +1,86 @@
 package com.github.tvbox.osc.base;
 
-import android.app.Activity;
-import androidx.multidex.MultiDexApplication;
-
-import com.github.tvbox.osc.bean.VodInfo;
-import com.github.tvbox.osc.callback.EmptyCallback;
-import com.github.tvbox.osc.callback.LoadingCallback;
-import com.github.tvbox.osc.data.AppDataManager;
-import com.github.tvbox.osc.server.ControlManager;
-import com.github.tvbox.osc.util.AppManager;
-import com.github.tvbox.osc.util.EpgUtil;
-import com.github.tvbox.osc.util.FileUtils;
+import android.app.Application;
+import android.content.Context;
+import androidx.multidex.MultiDex;
 import com.github.tvbox.osc.util.HawkConfig;
-import com.github.tvbox.osc.util.LOG;
-import com.github.tvbox.osc.util.OkGoHelper;
-import com.github.tvbox.osc.util.PlayerHelper;
-import com.kingja.loadsir.core.LoadSir;
 import com.orhanobut.hawk.Hawk;
-import com.p2p.P2PClass;
-import com.whl.quickjs.android.QuickJSLoader;
-import com.github.catvod.crawler.JsLoader;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
-import me.jessyan.autosize.AutoSizeConfig;
-import me.jessyan.autosize.unit.Subunits;
-
-/**
- * @author pj567
- * @date :2020/12/17
- * @description:
- */
-public class App extends MultiDexApplication {
+public class App extends Application {
     private static App instance;
 
-    private static P2PClass p;
-    public static String burl;
-    private static String dashData;
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // 关键：Dalvik 虚拟机在 Android 4.2 必须首先初始化 MultiDex
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
-        initParams();
-        // OKGo
-        OkGoHelper.init(); //台标获取
-        EpgUtil.init();
-        // 初始化Web服务器
-        ControlManager.init(this);
-        //初始化数据库
-        AppDataManager.init();
-        LoadSir.beginBuilder()
-                .addCallback(new EmptyCallback())
-                .addCallback(new LoadingCallback())
-                .commit();
-        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
-                .setSupportDP(false)
-                .setSupportSP(false)
-                .setSupportSubunits(Subunits.MM);
-        PlayerHelper.init();
-        QuickJSLoader.init();
-        FileUtils.cleanPlayerCache();
-    }
-
-    private void initParams() {
-        // Hawk
         Hawk.init(this).build();
-        Hawk.put(HawkConfig.DEBUG_OPEN, false);
-        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 1);
-        }
+        initLegacySSL();
+        initDefaultConfigs();
     }
 
     public static App getInstance() {
         return instance;
     }
 
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        JsLoader.destroy();
-    }
-
-
-    private VodInfo vodInfo;
-    public void setVodInfo(VodInfo vodinfo){
-        this.vodInfo = vodinfo;
-    }
-    public VodInfo getVodInfo(){
-        return this.vodInfo;
-    }
-
-    public static P2PClass getp2p() {
+    /**
+     * 解决 Android 4.2.2 默认不支持 TLS 1.2 以及老旧根证书过期的 HTTPS 握手失败问题
+     */
+    private void initLegacySSL() {
         try {
-            if (p == null) {
-                p = new P2PClass(FileUtils.getExternalCachePath());
-            }
-            return p;
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[]{};
+                    }
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
         } catch (Exception e) {
-            LOG.e(e.toString());
-            return null;
+            e.printStackTrace();
         }
     }
 
-    public Activity getCurrentActivity() {
-        return AppManager.getInstance().currentActivity();
-    }
-
-    public void setDashData(String data) {
-        dashData = data;
-    }
-    public String getDashData() {
-        return dashData;
+    /**
+     * 设置适合 x86 老平板的默认硬件/解码参数
+     */
+    private void initDefaultConfigs() {
+        // 默认播放器: 1 为 IJK 播放器 (0:系统默认, 1:IJK, 2:Exo)
+        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);
+        }
+        // 默认解码格式: 软解码 (x86 CPU 软解能力强于 4.2 老系统失效的硬解码)
+        if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
+            Hawk.put(HawkConfig.IJK_CODEC, "软解码");
+        }
+        // 默认主页推荐
+        if (!Hawk.contains(HawkConfig.HOME_REC)) {
+            Hawk.put(HawkConfig.HOME_REC, 1);
+        }
     }
 }

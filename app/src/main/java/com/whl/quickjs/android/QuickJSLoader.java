c434d0eacf0ec426971c821d495ec0d9667af7b7
Erisfashion <erisfashion@gmail.com>
2026-09-18 13:43:03 +0800

    Add QuickJSLoader class with initialization methods

---
+++ app/src/main/java/com/whl/quickjs/android/QuickJSLoader.java
@@ -0,0 +1,9 @@
+package com.whl.quickjs.android;
+
+import android.content.Context;
+
+public class QuickJSLoader {
+    public static void init() {}
+    public static void init(Context context) {}
+    public static void initConsole() {}
+}

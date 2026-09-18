bb88a365860ed0375787386d5ef6e8f8ae9dc13a
Erisfashion <erisfashion@gmail.com>
2026-09-18 20:12:05 +0800

    Refactor ApiHistoryDialog and update adapter handling
    Refactor ApiHistoryDialog to use mGridView and add new setAdapter methods.

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiHistoryDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiHistoryDialog.java
@@ -1,25 +1,18 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
-import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.ui.adapter.ApiHistoryDialogAdapter;
 import com.owen.tvrecyclerview.widget.TvRecyclerView;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApiHistoryDialog extends BaseDialog {
     private TextView tvTitle;
-    private TvRecyclerView tvRecyclerView;
+    private TvRecyclerView mGridView;
     private ApiHistoryDialogAdapter adapter;
-    private HistoryCallback callback;
-
-    public interface HistoryCallback {
-        void select(int position);
-        void delete(int position);
-    }
 
     public ApiHistoryDialog(@NonNull Context context) {
         super(context);
@@ -29,34 +22,35 @@ public class ApiHistoryDialog extends BaseDialog {
 
     private void initView() {
         tvTitle = findViewById(R.id.tvTitle);
-        tvRecyclerView = findViewById(R.id.tvRecyclerView);
+        mGridView = findViewById(R.id.mGridView);
     }
 
-    public ApiHistoryDialog setData(List<String> data, int select, HistoryCallback callback) {
-        this.callback = callback;
-        adapter = new ApiHistoryDialogAdapter(data);
-        adapter.setSelect(select);
-        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter a, View view, int position) {
-                if (ApiHistoryDialog.this.callback != null) {
-                    ApiHistoryDialog.this.callback.select(position);
-                }
-                dismiss();
-            }
-        });
-        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
-            @Override
-            public void onItemChildClick(BaseQuickAdapter a, View view, int position) {
-                if (view.getId() == R.id.ivDelete) {
-                    if (ApiHistoryDialog.this.callback != null) {
-                        ApiHistoryDialog.this.callback.delete(position);
-                    }
-                }
-            }
-        });
-        tvRecyclerView.setAdapter(adapter);
-        tvRecyclerView.setSelection(select);
+    public ApiHistoryDialog setTip(String tip) {
+        if (tvTitle != null) {
+            tvTitle.setText(tip);
+        }
         return this;
+    }
+
+    public ApiHistoryDialog setAdapter(ApiHistoryDialogAdapter.SelectDialogInterface dialogInterface, ArrayList<String> data, int select) {
+        adapter = new ApiHistoryDialogAdapter(dialogInterface);
+        adapter.setData(data, select);
+        if (mGridView != null) {
+            mGridView.setAdapter(adapter);
+            mGridView.setSelection(select);
+        }
+        return this;
+    }
+
+    public ApiHistoryDialog setAdapter(ApiHistoryDialogAdapter.SelectDialogInterface dialogInterface, List<String> data, int select) {
+        ArrayList<String> list = new ArrayList<>();
+        if (data != null) {
+            list.addAll(data);
+        }
+        return setAdapter(dialogInterface, list, select);
+    }
+
+    public ApiHistoryDialogAdapter getAdapter() {
+        return adapter;
     }
 }

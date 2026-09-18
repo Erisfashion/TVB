834b327fa8a2d9e48c65b614c046e6ec3aab25f6
Erisfashion <erisfashion@gmail.com>
2026-09-18 20:01:38 +0800

    Refactor ApiHistoryDialog to improve structure and callbacks

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiHistoryDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiHistoryDialog.java
@@ -1,45 +1,62 @@
 package com.github.tvbox.osc.ui.dialog;
 
 import android.content.Context;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.ui.adapter.ApiHistoryDialogAdapter;
 import com.owen.tvrecyclerview.widget.TvRecyclerView;
-
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 
 public class ApiHistoryDialog extends BaseDialog {
-    public ApiHistoryDialog(@NonNull @NotNull Context context) {
-        super(context, R.style.CustomDialogStyleDim);
+    private TextView tvTitle;
+    private TvRecyclerView tvRecyclerView;
+    private ApiHistoryDialogAdapter adapter;
+    private HistoryCallback callback;
+
+    public interface HistoryCallback {
+        void select(int position);
+        void delete(int position);
+    }
+
+    public ApiHistoryDialog(@NonNull Context context) {
+        super(context);
         setContentView(R.layout.dialog_api_history);
+        initView();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private void initView() {
+        tvTitle = findViewById(R.id.tvTitle);
+        tvRecyclerView = findViewById(R.id.tvRecyclerView);
     }
 
-    public void setTip(String tip) {
-        ((TextView) findViewById(R.id.title)).setText(tip);
-    }
-
-    public void setAdapter(ApiHistoryDialogAdapter.SelectDialogInterface sourceBeanSelectDialogInterface, List<String> data, int select) {
-        ApiHistoryDialogAdapter adapter = new ApiHistoryDialogAdapter(sourceBeanSelectDialogInterface);
-        adapter.setData(data, select);
-        TvRecyclerView tvRecyclerView = ((TvRecyclerView) findViewById(R.id.list));
-        tvRecyclerView.setAdapter(adapter);
-        tvRecyclerView.setSelectedPosition(select);
-        tvRecyclerView.post(new Runnable() {
+    public ApiHistoryDialog setData(List<String> data, int select, HistoryCallback callback) {
+        this.callback = callback;
+        adapter = new ApiHistoryDialogAdapter(data);
+        adapter.setSelect(select);
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
-            public void run() {
-                tvRecyclerView.scrollToPosition(select);
+            public void onItemClick(BaseQuickAdapter a, View view, int position) {
+                if (ApiHistoryDialog.this.callback != null) {
+                    ApiHistoryDialog.this.callback.select(position);
+                }
+                dismiss();
             }
         });
+        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter a, View view, int position) {
+                if (view.getId() == R.id.ivDelete) {
+                    if (ApiHistoryDialog.this.callback != null) {
+                        ApiHistoryDialog.this.callback.delete(position);
+                    }
+                }
+            }
+        });
+        tvRecyclerView.setAdapter(adapter);
+        tvRecyclerView.setSelection(select);
+        return this;
     }
 }

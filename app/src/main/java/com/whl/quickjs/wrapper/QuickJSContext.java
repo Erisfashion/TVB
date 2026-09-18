ee841e1ee5d4a6f4842559fc38327bcc8f62029e
Erisfashion <erisfashion@gmail.com>
2026-09-18 14:08:28 +0800

    Add new methods to QuickJSContext class

--- app/src/main/java/com/whl/quickjs/wrapper/QuickJSContext.java
+++ app/src/main/java/com/whl/quickjs/wrapper/QuickJSContext.java
@@ -26,4 +26,18 @@ public class QuickJSContext {
     public JSArray createNewJSArray() {
         return new JSArray();
     }
+
+    public JSFunction createNewJSFunction(JSCallFunction function) {
+        return new JSFunction();
+    }
+
+    public void registerJavaMethod(Object target, String methodName) {}
+    public void registerJavaMethod(JSCallFunction handler, String name) {}
+    public void registerJavaMethod(Object target, String methodName, String jsMethodName) {}
+    public void registerAll(Object target) {}
+    public void initConsole() {}
+    public void throwJSException(String message) {}
+    public Object getUndefined() { return null; }
+    public Object getNull() { return null; }
+    public String stringify(JSObject jsObject) { return "{}"; }
 }

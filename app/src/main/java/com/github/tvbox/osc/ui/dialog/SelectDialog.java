8e8ae7b5b9504dd50a92220ac78b852f3ea89e1e
Erisfashion <erisfashion@gmail.com>
2026-09-18 20:11:37 +0800

    Refactor SelectDialog for generics and adapter updates
    Refactor SelectDialog to support generic types and update adapter methods.

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/SelectDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/SelectDialog.java
@@ -4,21 +4,16 @@ import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.ui.adapter.SelectDialogAdapter;
 import com.owen.tvrecyclerview.widget.TvRecyclerView;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SelectDialog extends BaseDialog {
+public class SelectDialog<T> extends BaseDialog {
     private TextView tvTitle;
-    private TvRecyclerView tvRecyclerView;
-    private SelectDialogAdapter adapter;
-    private SelectCallback callback;
-
-    public interface SelectCallback {
-        void select(int position);
-    }
+    private TvRecyclerView mGridView;
+    private SelectDialogAdapter<T> adapter;
 
     public SelectDialog(@NonNull Context context) {
         super(context);
@@ -28,29 +23,47 @@ public class SelectDialog extends BaseDialog {
 
     private void initView() {
         tvTitle = findViewById(R.id.tvTitle);
-        tvRecyclerView = findViewById(R.id.tvRecyclerView);
+        mGridView = findViewById(R.id.mGridView);
     }
 
-    public SelectDialog setTitle(String title) {
-        tvTitle.setText(title);
+    public SelectDialog<T> setTip(String tip) {
+        if (tvTitle != null) {
+            tvTitle.setText(tip);
+        }
         return this;
     }
 
-    public SelectDialog setData(List<String> data, int select, SelectCallback callback) {
-        this.callback = callback;
-        adapter = new SelectDialogAdapter(data);
-        adapter.setSelect(select);
-        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter a, View view, int position) {
-                if (SelectDialog.this.callback != null) {
-                    SelectDialog.this.callback.select(position);
-                }
-                dismiss();
-            }
-        });
-        tvRecyclerView.setAdapter(adapter);
-        tvRecyclerView.setSelection(select);
+    public SelectDialog<T> setTitle(String title) {
+        return setTip(title);
+    }
+
+    public SelectDialog<T> setAdapter(SelectDialogAdapter.SelectDialogInterface<T> dialogInterface, SelectDialogAdapter.ItemCallback<T> itemCallback, ArrayList<T> data, int select) {
+        adapter = new SelectDialogAdapter<>(dialogInterface, itemCallback);
+        adapter.setData(data, select);
+        if (mGridView != null) {
+            mGridView.setAdapter(adapter);
+            mGridView.setSelection(select);
+        }
         return this;
+    }
+
+    public SelectDialog<T> setAdapter(SelectDialogAdapter.SelectDialogInterface<T> dialogInterface, SelectDialogAdapter.ItemCallback<T> itemCallback, List<T> data, int select) {
+        ArrayList<T> list = new ArrayList<>();
+        if (data != null) {
+            list.addAll(data);
+        }
+        return setAdapter(dialogInterface, itemCallback, list, select);
+    }
+
+    public SelectDialog<T> setAdapter(SelectDialogAdapter<T> adapter) {
+        this.adapter = adapter;
+        if (mGridView != null) {
+            mGridView.setAdapter(adapter);
+        }
+        return this;
+    }
+
+    public SelectDialogAdapter<T> getAdapter() {
+        return adapter;
     }
 }

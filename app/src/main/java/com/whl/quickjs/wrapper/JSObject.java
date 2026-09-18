8d997ea840cf026ac3222d435667dd717f38a380
Erisfashion <erisfashion@gmail.com>
2026-09-18 14:08:54 +0800

    Add methods to retrieve int, long, and double values

--- app/src/main/java/com/whl/quickjs/wrapper/JSObject.java
+++ app/src/main/java/com/whl/quickjs/wrapper/JSObject.java
@@ -23,6 +23,27 @@ public class JSObject {
         return null;
     }
 
+    public int getInt(String key) {
+        Integer val = getInteger(key);
+        return val == null ? 0 : val;
+    }
+
+    public long getLong(String key) {
+        Object val = get(key);
+        if (val instanceof Number) {
+            return ((Number) val).longValue();
+        }
+        return 0L;
+    }
+
+    public double getDouble(String key) {
+        Object val = get(key);
+        if (val instanceof Number) {
+            return ((Number) val).doubleValue();
+        }
+        return 0.0;
+    }
+
     public Boolean getBoolean(String key) {
         Object val = get(key);
         return val instanceof Boolean ? (Boolean) val : false;
@@ -54,4 +75,8 @@ public class JSObject {
     public void put(String key, Object val) {
         properties.put(key, val);
     }
+
+    public void registerJavaMethod(Object target, String methodName) {}
+    public void registerJavaMethod(JSCallFunction handler, String name) {}
+    public void registerAll(Object target) {}
 }

622abdbb68ced24445339ed3500588bc1bfaf1ed
Erisfashion <erisfashion@gmail.com>
2026-09-18 15:10:04 +0800

    Refactor JSUtils to support generic types

--- app/src/main/java/com/whl/quickjs/wrapper/JSUtils.java
+++ app/src/main/java/com/whl/quickjs/wrapper/JSUtils.java
@@ -1,10 +1,33 @@
 package com.whl.quickjs.wrapper;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class JSUtils {
+public class JSUtils<T> {
+
+    public JSObject toObj(QuickJSContext ctx, Map<String, T> map) {
+        JSObject obj = ctx != null ? ctx.createJSObject() : new JSObject();
+        if (map != null) {
+            for (Map.Entry<String, T> entry : map.entrySet()) {
+                obj.set(entry.getKey(), entry.getValue());
+            }
+        }
+        return obj;
+    }
+
+    public JSArray toArray(QuickJSContext ctx, Collection<T> collection) {
+        JSArray array = ctx != null ? ctx.createJSArray() : new JSArray();
+        if (collection != null) {
+            int i = 0;
+            for (T item : collection) {
+                array.set(i++, item);
+            }
+        }
+        return array;
+    }
+
     public static String stringify(QuickJSContext context, JSObject jsObject) {
         return jsObject != null ? jsObject.toString() : "{}";
     }
@@ -26,7 +49,7 @@ public class JSUtils {
     }
 
     public static Object parse(QuickJSContext context, String json) {
-        return new JSObject();
+        return context != null ? context.parse(json) : new JSObject();
     }
 
     public static Object parse(String json) {
